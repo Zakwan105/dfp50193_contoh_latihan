3109d47a0f943a8f3041557aad9d9205f0d537cb
Zakwan105 <khairulzakwan105@gmail.com>
2021-10-21 11:04:59 +0800

    contoh login

--- conn.php
+++ conn.php
@@ -1,7 +1,7 @@
 <?php
 $host = 'localhost';
 $user = 'root';
-$pswd = '';
+$pswd = ''; # sepasang single quotes
 $dbname = 'contoh_login';
-$conn = new mysqli($host . $user, $pswd, $dbname);
-session_start();
+
+$conn = new mysqli($host, $user, $pswd, $dbname); session_start();
